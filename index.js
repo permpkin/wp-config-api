622eab67dfffb4ce0e6cca58275112f033df120e
Peter Kavanagh <peter@tensorgroup.org>
2021-03-03 00:51:31 +1100

    rejig base route

--- index.js
+++ index.js
@@ -52,11 +52,7 @@ App.use(express.json());
 /**
  * Respond to base route
  */
-App.get('/', (req, res) => {
-  
-  res.sendStatus(200)
-
-})
+App.get('/', (req, res) => res.sendStatus(200))
 
 /**
  * Inject config json for modification.
