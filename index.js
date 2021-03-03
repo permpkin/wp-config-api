aa7cb54ac019ae8bf9168d34fd51cb05f4ff5974
Peter Kavanagh <peter@tensorgroup.org>
2021-03-03 15:29:17 +1100

    revert cors changes

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ App.use(express.json());
  */
 App.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header("Access-Control-Allow-Headers", '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,HEAD');
+  res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
