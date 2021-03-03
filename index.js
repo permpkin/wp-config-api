f0469fb6c4f37c3d5fbf2c461164875bb99e364d
Peter Kavanagh <peter@tensorgroup.org>
2021-03-03 13:10:07 +1100

    add content type header

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ App.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", 'Content-Type');
   next();
 });
 
