73556b6809907318734fa73794f6601cda982c2a
Peter Kavanagh <peter@tensorgroup.org>
2021-03-03 13:08:30 +1100

    remove header sets

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ App.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 
