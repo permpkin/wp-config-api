02fc6166738eaca1cf34dc7f813ac7eb446d5ff5
Peter Kavanagh <peter@tensorgroup.org>
2021-03-03 15:17:39 +1100

    move around those dang cors

--- index.js
+++ index.js
@@ -57,9 +57,8 @@ App.use(express.json());
  */
 App.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,HEAD');
-  res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header("Access-Control-Allow-Headers", '*');
   next();
 });
 
