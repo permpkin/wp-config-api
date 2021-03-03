cbb40635bbf822d3f54b29ba22387d87991e68c2
Peter Kavanagh <peter@tensorgroup.org>
2021-03-03 15:15:18 +1100

    add HEAD to cors

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ App.use(express.json());
 App.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,HEAD');
   res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
