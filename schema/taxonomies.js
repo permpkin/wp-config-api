d39f810dd54f81e1916f2a6bda1804240a7176f8
Peter Kavanagh <peter@tensorgroup.org>
2021-03-14 17:14:41 +1100

    update taxonomy schema

--- schema/taxonomies.js
+++ schema/taxonomies.js
@@ -4,33 +4,51 @@ const Joi = require('joi');
 const schema = Joi.object({
   
   key: Joi.string().required(),
-  label: Joi.string(),
+  
   object_type: Joi.array().items(Joi.string()),
-  description: Joi.string(),
   labels: Joi.object({
     name: Joi.string(),
     singular_name: Joi.string(),
     menu_name: Joi.string(),
     name_admin_bar: Joi.string(),
   }),
-  supports: Joi.array().items(Joi.string()),
-  menu_icon: Joi.string(),
-  heirarchical: Joi.boolean(),
+  description: Joi.string(),
   public: Joi.boolean(),
+  publicly_queryable: Joi.boolean(),
+  hierarchical: Joi.boolean(),
   show_ui: Joi.boolean(),
   show_in_menu: Joi.boolean(),
-  menu_position: Joi.number(),
-  show_in_admin_bar: Joi.boolean(),
   show_in_nav_menus: Joi.boolean(),
   show_in_rest: Joi.boolean(),
-  can_export: Joi.boolean(),
-  has_archive: Joi.boolean(),
-  exclude_from_search: Joi.boolean(),
-  publicly_queryable: Joi.boolean(),
-  rewrite: Joi.boolean(),
-  delete_with_user: Joi.boolean(),
-  capability_type: Joi.string(),
-  hierarchical: Joi.boolean()
+  rest_base: Joi.string(),
+  rest_controller_class: Joi.string(),
+  show_tagcloud: Joi.boolean(),
+  show_in_quick_edit: Joi.boolean(),
+  show_admin_column: Joi.boolean(),
+  meta_box_cb: Joi.string(),
+  meta_box_sanitize_cb: Joi.string(),
+  capabilities: Joi.array().items(Joi.string()), // TODO: update to match
+  rewrite: Joi.any(
+    Joi.boolean(),
+    Joi.object({
+      slug: Joi.string(),
+      with_front: Joi.boolean(),
+      hierarchical: Joi.boolean(),
+      ep_mask: Joi.number(),
+    })
+  ),
+  query_var: Joi.string(),
+  update_count_callback: Joi.string(),
+  default_term: Joi.any(
+    Joi.string(),
+    Joi.object({
+      name: Joi.string(),
+      slug: Joi.string(),
+      description: Joi.string()
+    })
+  ),
+  sort: Joi.boolean(),
+  args: Joi.array().items(Joi.string())
 
 })
 
